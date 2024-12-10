aa3b2d5dd1885bfc5ffc31d3e83f8fd556e954f6
ettabet aicha <aicha70tabite@gmail.com>
2024-12-10 18:31:04 +0100

    add endpoint to test the logout

--- src/main/java/org/aicha/hunters_leagues/web/api/AuthAPI.java
+++ src/main/java/org/aicha/hunters_leagues/web/api/AuthAPI.java
@@ -12,13 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping
 @RequiredArgsConstructor
 public class AuthAPI {
 
     private final UserService userService;
     private final UserVmMapper userVmMapper;
-    // method to login user
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(
             @Valid @RequestBody LoginRequest loginRequest) {
@@ -26,12 +26,22 @@ public class AuthAPI {
         return ResponseEntity.ok(AuthResponse.builder().token(user.getToken()).build());
     }
 
-    // method to register user
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(
             @Valid @RequestBody RegisterRequest registerRequest) {
         User userEntity = userVmMapper.toUser(registerRequest);
         User user = userService.addSUser(userEntity);
         return ResponseEntity.ok(AuthResponse.builder().token(user.getToken()).build());
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        return ResponseEntity.noContent().build();
+    }
+
+    //  of adding a GET endpoint for login
+    @GetMapping("/")
+    public ResponseEntity<String> loginPage() {
+        return ResponseEntity.ok("Login page");
     }
 }
